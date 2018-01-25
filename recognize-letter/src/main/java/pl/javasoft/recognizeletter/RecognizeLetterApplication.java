71e128db8fe7db557d3f4238465005928b76e2ce
Krzysztof Pobożan <krzpob@gmail.com>
2018-01-25 23:51:30 +0100

    First commit

---
+++ recognize-letter/src/main/java/pl/javasoft/recognizeletter/RecognizeLetterApplication.java
@@ -0,0 +1,12 @@
+package pl.javasoft.recognizeletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognizeLetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecognizeLetterApplication.class, args);
+	}
+}
